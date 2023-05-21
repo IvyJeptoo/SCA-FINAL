3e43daa29ea18119d574c57ab6fa35e8b60a417c
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2023-05-21 04:14:47 +0300

    set up angular listing project with routing,firebase and uthentication

---
+++ FRONT/buy-and-sell/src/app/my-listings/my-listings.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ListingsService } from '../listings.service'
+import { fakeMyListings } from '../fake-data';
+import { Listing } from '../types';
+
+@Component({
+  selector: 'app-my-listings',
+  templateUrl: './my-listings.component.html',
+  styleUrls: ['./my-listings.component.css']
+})
+export class MyListingsComponent implements OnInit {
+  listings: Listing[] = [];
+
+
+  constructor(
+    private listingsService: ListingsService
+  ) { }
+
+  ngOnInit(): void {
+    this.listingsService.getListingsForUser()
+    .subscribe(listings => this.listings =listings)
+  }
+
+  onDeleteClicked(listingId: string | null): void{
+    this.listingsService.deleteListing(listingId)
+    .subscribe(() => {
+      this.listings =this.listings.filter(
+        listing => listing.id !==listingId
+      );
+    });
+
+  }
+
+}
---
+++ FRONT/buy-and-sell/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCH47EMsjHdV9BKTqFFbqAl7wCGVgUfflY",
+    authDomain: "buy-and-sell-27dd1.firebaseapp.com",
+    projectId: "buy-and-sell-27dd1",
+    storageBucket: "buy-and-sell-27dd1.appspot.com",
+    messagingSenderId: "626235023178",
+    appId: "1:626235023178:web:b1b41a196929b0dd13ebb3",
+    measurementId: "G-7WED2PRV7C"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
